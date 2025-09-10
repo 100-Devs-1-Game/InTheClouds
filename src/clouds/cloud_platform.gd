b674aa7e2482fcfb194c21f76ecfd1301416e63b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-10 22:37:08 +0200

    Remove left overs

--- src/clouds/cloud_platform.gd
+++ src/clouds/cloud_platform.gd
@@ -1,14 +1,9 @@
 class_name WindPlatformerMinigameCloudPlatform
 extends AnimatableBody2D
 
-@export var fade_duration: float = 2.5
-
 var speed := randf_range(-50, 50)
-var fade_tween: Tween
 
 @onready var parts: Node2D = $Parts
-@onready var label_multiplier_2x: Label = $"Label Multiplier 2x"
-@onready var label_multiplier_5x: Label = $"Label Multiplier 5x"
 @onready var full_cloud: Sprite2D = $FullCloud
 
 
@@ -27,27 +22,9 @@ func set_parts_areas_active(b: bool):
 		part.active = b
 
 
-func fade():
-	collision_layer = 0
-	if fade_tween:
-		return
-	assert(parts.get_child_count() > 0)
-	set_parts_areas_active(false)
-	parts.queue_free()
-	full_cloud.show()
-
-	fade_tween = create_tween()
-	fade_tween.tween_property(self, "modulate", Color.TRANSPARENT, fade_duration)
-	fade_tween.tween_callback(queue_free)
-
-
 func _on_player_detection_area_body_entered(_body: Node2D) -> void:
 	set_parts_areas_active(true)
 
 
 func _on_player_detection_area_body_exited(_body: Node2D) -> void:
 	set_parts_areas_active(false)
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
