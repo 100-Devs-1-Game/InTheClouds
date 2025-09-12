b74c80204ad423c1f9584f1ed3ea98c113b57e3b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-12 23:50:39 +0200

    Update scene_loader.gd

--- src/global/scene loader/scene_loader.gd
+++ src/global/scene loader/scene_loader.gd
@@ -2,13 +2,21 @@ extends Node
 
 @export var menu_scene: PackedScene
 @export var game_scene: PackedScene
+@export var skip_to_game: bool= false
 
+
+
+func _ready() -> void:
+	if skip_to_game:
+		enter_game()
+	else:
+		enter_menu()
 
 
 func enter_menu():
 	if Global.elapsed_game_time > 0:
 		SaveManager.post_highscore(Global.elapsed_game_time)
-	get_tree().change_scene_to_packed(menu_scene)
+	get_tree().change_scene_to_packed.call_deferred(menu_scene)
 
 
 func enter_game():
