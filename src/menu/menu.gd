ac2680cf81aaed5c7687ffaafe17e6819aca75c3
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-12 23:25:09 +0200

    Add highscores panel

--- src/menu/menu.gd
+++ src/menu/menu.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@export var highscore_label_settings: LabelSettings
+
 @onready var cloud_title: Sprite2D = $CanvasLayer/CloudTitle
 @onready var menu: PanelContainer = $CanvasLayer/Menu
 @onready var clouds: Node2D = $CanvasLayer/Clouds
 @onready var buttons_container: VBoxContainer = %"VBoxContainer Buttons"
+@onready var vbox_highscores: VBoxContainer = %"VBoxContainer Highscores"
 
 
 func _ready() -> void:
@@ -11,20 +14,14 @@ func _ready() -> void:
 		button.text= " ".join(button.text.split())
 	
 	for cloud in clouds.get_children():
-		#var new_tween:= create_tween()
-		#new_tween.tween_property(cloud, "position:y", 10, 2.0).as_relative().set_ease(Tween.EASE_IN_OUT)
-		#new_tween.tween_property(cloud, "position:y", -10, 2.0).as_relative().set_ease(Tween.EASE_IN_OUT)
-		#new_tween.set_loops()
 		cloud.set_meta("delta", randf() * 10)
 		cloud.set_meta("orig_y", cloud.position.y)
 
-	#set_process(false)
 	cloud_title.show()
-
-	#not working
-	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	
 	var duration:= 1.5
+	if SceneLoader.has_return_from_game():
+		duration= 0.5
 	
 	var tween:= create_tween()
 	tween.tween_property(cloud_title, "position:y", 250, duration)
@@ -36,15 +33,12 @@ func _ready() -> void:
 	tween.tween_property(menu, "modulate:a", 1.0, duration - 0.5).set_delay(0.5)
 	tween.tween_callback(wiggle_clouds)
 
+	update_highscores()
+
 
 func wiggle_clouds():
-	#var tween:= create_tween()
-	#tween.tween_property(cloud_title, "position:y", 10, 2.0).as_relative().set_ease(Tween.EASE_IN_OUT)
-	#tween.tween_property(cloud_title, "position:y", -10, 2.0).as_relative().set_ease(Tween.EASE_IN_OUT)
-	#tween.set_loops()
-
-
 	set_process(true)
+
 
 func _process(delta: float) -> void:
 	var freq:= 1.0
@@ -52,3 +46,22 @@ func _process(delta: float) -> void:
 	for cloud in clouds.get_children():
 		cloud.position.y= sin(cloud.get_meta("delta") * freq) * amp + cloud.get_meta("orig_y")
 		cloud.set_meta("delta", cloud.get_meta("delta") + delta)
+
+
+func update_highscores():
+	while vbox_highscores.get_child_count() > 0:
+		var child:= vbox_highscores.get_child(0)
+		vbox_highscores.remove_child(child)
+		child.queue_free()
+	
+	for i in SaveManager.NUM_SCORES:
+		var label:= Label.new()
+		var s:= ""
+		if SaveManager.highscores.size() > i:
+			s= Utils.get_time_string(SaveManager.highscores[i]) + "s"
+		else:
+			s= "---"
+
+		label.label_settings= highscore_label_settings
+		label.text= str(i + 1, ": ", s)
+		vbox_highscores.add_child(label)
