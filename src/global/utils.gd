4f1673d9c21a7656ad6186ae4346c4f49c1b728c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-13 03:39:58 +0200

    Update utils.gd

--- src/global/utils.gd
+++ src/global/utils.gd
@@ -2,4 +2,4 @@ class_name Utils
 
 
 static func get_time_string(secs: float)-> String:
-	return "%d:%02d:%1d" % [ int(secs / 60), int(secs) % 60, (secs - int(secs)) * 10 ]	
+	return "%d:%02d.%1d" % [ int(secs / 60), int(secs) % 60, (secs - int(secs)) * 10 ]	
